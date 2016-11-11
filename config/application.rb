728c062960a26706a32cd9a6279b1c25c995360d
tadashi morizane <t.morizane1954@gmail.com>
2016-11-11 09:48:03 +0000

    add sign sign up and user detail

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,7 @@ module Microposts
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    # config.i18n.default_locale = :ja
   end
 end
