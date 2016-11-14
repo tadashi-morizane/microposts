64702b35e29fe3438cd11e4b77dd7ddece777f12
tadashi morizane <t.morizane1954@gmail.com>
2016-11-14 03:55:13 +0000

    add edit profile2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      logged_in_user @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user # ここを修正
     else
