480d7027cd389a4cfd1d67d327659e46ef2d5e69
tadashi morizane <t.morizane1954@gmail.com>
2016-11-18 06:31:55 +0000

    add following-follower list-2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#ユーザーの作成
+user_a = User.create(name: "user_a", email: "user_a@gmail.com", password: "abcdefgh")
+user_b = User.create(name: "user_b", email: "user_b@gmail.com", password: "abcdefgh")
+user_c = User.create(name: "user_c", email: "user_c@gmail.com", password: "abcdefgh")
+
+#ユーザーAはBとCをフォロー
+#ユーザーBはAをフォロー
+#ユーザーCは誰もフォローしてません
+Relationship.create(follower_id: user_a.id, followed_id: user_b.id)
+Relationship.create(follower_id: user_a.id, followed_id: user_c.id)
+Relationship.create(follower_id: user_b.id, followed_id: user_a.id)
+
+#ユーザーAとBの発言を５件ずつ作成
+5.times do |no|
+  Micropost.create(user_id: user_a.id, content: "UserAの発言#{no}")
+  Micropost.create(user_id: user_b.id, content: "UserBの発言#{no}")
+end
